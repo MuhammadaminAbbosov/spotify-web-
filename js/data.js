488301d453a75f7a3aa6748d5d761b157e265edd
MuhammadaminAbbosov <musulmaanjanob@gmail.com>
2022-01-28 13:21:08 +0500

    complated

--- js/data.js
+++ js/data.js
@@ -8,3 +8,56 @@ const asideRightObjact = [
     "./assets/images/ar-7.png",
     "./assets/images/ar-8.png"
 ]
+
+const dailyImgs = [
+    "./assets/images/db-1.png",
+    "./assets/images/db-2.png",
+    "./assets/images/db-3.png"
+]
+
+const cardData = [
+    {
+        img : "./assets/images/cc-1.png",
+        musicName: "Where Are You Now",
+        author: "Lost Frequencies, Calum <br> Scott"
+    },
+    {
+        img : "./assets/images/cc-2.png",
+        musicName: "turn off the lights bae",
+        author: "By Iarin"
+    },
+    {
+        img : "./assets/images/cc-3.png",
+        musicName: "Red (Taylor’s Version)",
+        author: "Taylor Swift"
+    },
+    {
+        img : "./assets/images/cc-4.png",
+        musicName: "Until Now",
+        author: "Swedish House Mafia"
+    }
+]
+
+const cardData2 = [
+    {
+        img : "./assets/images/cc-5.png",
+        musicName: "Your Time Capsule",
+        author: "We made you a <br> personalized playlist..."
+    },
+    {
+        img : "./assets/images/cc-2.png",
+        musicName: "Best of the Decade...",
+        author: "A few of the many great <br> songs from the..."
+    },
+    {
+        img : "./assets/images/cc-6.png",
+        musicName: "Indie Mix",
+        author: "Florence + The Machine, <br> The Neigh..."
+    },
+    {
+        img : "./assets/images/cc-2.png",
+        musicName: "Discover Weekly",
+        author: "Your weekly mixtape of <br> fresh music. Enjoy..."
+    }
+
+]
--- js/index.js
+++ js/index.js
@@ -1,7 +1,53 @@
 const asideRight = document.querySelector(".aside-right")
-asideRightObjact.forEach((e) => {
+asideRightObjact.forEach( e => {
     const asideImg = document.createElement("img")
     asideImg.src = e
 
     asideRight.appendChild(asideImg)
+})
+
+
+const dBox = document.querySelector(".boxs")
+dailyImgs.forEach( e => {
+    const dImg = document.createElement("img")
+    dImg.src = e
+
+    dBox.appendChild(dImg)
+})
+
+const cardTop = document.querySelector(".cards")
+
+cardData.forEach(e => {
+    const card = document.createElement("div")
+    const cardImg = document.createElement("img")
+    const text = document.createElement("div")
+    const musicName = document.createElement("b") 
+    const author = document.createElement("p") 
+    
+    card.classList.add("card")
+    cardImg.src = e.img
+    musicName.innerHTML = e.musicName
+    author.innerHTML = e.author
+
+    text.append(musicName, author)
+    card.append(cardImg, text)
+    cardTop.appendChild(card)
+})
+
+const cardBottom = document.querySelector(".cards2")
+cardData2.forEach(e => {
+    const card = document.createElement("div")
+    const cardImg = document.createElement("img")
+    const text = document.createElement("div")
+    const musicName = document.createElement("b") 
+    const author = document.createElement("p") 
+    
+    card.classList.add("card")
+    cardImg.src = e.img
+    musicName.innerHTML = e.musicName
+    author.innerHTML = e.author
+
+    text.append(musicName, author)
+    card.append(cardImg, text)
+    cardBottom.appendChild(card)
 })
